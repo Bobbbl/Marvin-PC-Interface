4683a9796d8cbf6d38b28c3a71e4d54a859cd1c5
Bobbbl <sebastian.draxinger@googlemail.com>
2018-11-20 00:07:20 +0100

    Update

--- interface.py
+++ interface.py
@@ -5,6 +5,7 @@ import time
 import PyQt5
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtCore import QThread 
+from PyQt5.QtWidgets import QApplication, QWidget
 
 comm_dict = "__Start_Session__", "__End_Session__","__Send_Toolpath__", "__Empfang_Besteatigt__",  "__Receive_Error__", "__Receive_Successfull__"
 
@@ -13,40 +14,41 @@ comm_enum = {"Start_Session" : 0, "End_Session" : 1, "Start_Homing" : 2, "Send_T
 
 class Listener_Thread(QThread):
 
-    newMessage = QtCore.Signal(str)
-    readTimeout = QtCore.Signal()
+    newMessage = QtCore.pyqtSignal(str)
+    readTimeout = QtCore.pyqtSignal()
     write_flag = False
     read_flag = False
 
-    def __init__(self, port, read_timeout, write_timeout):
+    def __init__(self, port, read_timeout=None, write_timeout=None):
         QThread.__init__(self)  # Ruft den Mutterklassenkonstruktor auf
         self.port = serial.Serial(port, 115200, timeout=read_timeout, write_timeout=write_timeout)
-        self.seep(1)
+        self.sleep(1)
 
 
     def __del__(self):
         self.wait()
 
-    @QtCore.Slot(str)
-    def sendMessage(str)
+    def sendMessage(self, str):
         self.sendBuffer = str
         self.write_flag = True
     
-    @QtCore.Slot()
-    def receiveMessage():
+    def receiveMessage(self):
         self.read_flag = True
 
     def run(self):
         # Listener Code Here
         while(1):
-            if write_flag == True:
-                self.port.write(write_message)
+            if self.write_flag == True:
+                self.port.write(self.sendBuffer)
                 self.write_flag = False
-            elif read_flag == True:
+            elif self.read_flag == True:
                 msg = self.port.readline()
-                self.write_flag = False
                 if not msg:
                     self.readTimeout.emit()
+                    continue
+                self.newMessage.emit(str(msg))
+                if(msg.decode('ascii') == "__End_Session__\r\n"):
+                    self.read_flag = False
 
 
 
@@ -72,14 +74,26 @@ def list_serial_ports():
             pass
     return result
 
+def newMessageHandler(message):
+    print(message)
 
 if __name__ == '__main__':
+    app = QApplication(sys.argv)
     serial_ports_list = list_serial_ports()
     if not serial_ports_list:
         print("No Ports available")
     else:
-        #port.write(comm_dict[comm_enum["Start_Session"]].encode())
-        port.write(b"__Start_Session__")
+        port = Listener_Thread(serial_ports_list[0], read_timeout=1)
+        time.sleep(1)
+        port.newMessage.connect(newMessageHandler)
+        port.start()
+        port.sendMessage(b"__Start_Session__")
+        port.receiveMessage()
+        
+    sys.exit(app.exec_())
+
+
+
 
 
             
