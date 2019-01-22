315eea1aabc03ee7043cf0fcbf008f8afd524122
Sebastian Draxinger <sebastian.draxinger@googlemail.com>
2019-01-22 07:58:12 +0100

    v01

--- Tester_MainWindow.py
+++ Tester_MainWindow.py
@@ -1,6 +1,9 @@
 import sys
 import tester
 from PyQt5 import QtGui, QtWidgets, QtCore
+import serial
+import glob
+from interface import Listener_Thread
 
 
 
@@ -12,6 +15,19 @@ class Tester_Mainwindow(QtWidgets.QDialog, tester.Ui_Form):
         # Set Fixed Size W:250 H:230
         self.setFixedSize(250, 230)
 
+        # Pre
+        self.PreFeed = 100
+
+        # Serial Port
+        serial_ports_list = self.list_serial_ports()
+        if not serial_ports_list:
+            print("No Ports Available")
+        else:
+          self._Port = Listener_Thread(serial_ports_list[0], read_timeout=7)
+          self._Port.start()
+
+
+
         # Connections
         # Buttons
         self.pushButtonDown.clicked(self.pushButtonDown_Clicked)
@@ -21,16 +37,40 @@ class Tester_Mainwindow(QtWidgets.QDialog, tester.Ui_Form):
         self.pushButtonSend.clicked(self.pushButtonSend_Clicked)
 
     def pushButtonDown_Clicked(self):
-        pass
+        self._Port.sendMessage("XYF 0 -100 " + self.PreFeed)
 
     def pushButtonUp_Clicked(self):
-        pass
+        self._Port.sendMessage("XYF 0 100 " + self.PreFeed)
 
     def pushButtonLeft_Clicked(self):
-        pass
+        self._Port.sendMessage("XYF -100 0 " + self.PreFeed)
 
     def pushButtonRight_Clicked(self):
-        pass
+        self._Port.sendMessage("XYF 100 0 " + self.PreFeed)
 
     def pushButtonSend_Clicked(self):
-        pass
+        x = self.lineEditX.text()
+        y = self.lineEditY.text()
+        self._Port.sendMessage("XYF " + x + " " + y + self.PreFeed)
+
+
+    def list_serial_ports(self):
+        if sys.platform.startswith('win'):
+            ports = ['COM%s' % (i + 1) for i in range(256)]
+        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+            # this excludes your current terminal "/dev/tty"
+            ports = glob.glob('/dev/tty[A-Za-z]*')
+        elif sys.platform.startswith('darwin'):
+            ports = glob.glob('/dev/tty.*')
+        else:
+            raise EnvironmentError('Unsupported platform')
+
+        result = []
+        for port in ports:
+            try:
+                s = serial.Serial(port)
+                s.close()
+                result.append(port)
+            except (OSError, serial.SerialException):
+                pass
+        return result
